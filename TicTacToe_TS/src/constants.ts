c785ebe54acc3652450555b24d37f163006b78b5
Ludovic GOUYOU <ludovic.gouyou@zenika.com>
2018-09-09 19:19:42 +0200

    Initial Commit

--- TicTacToe_TS/src/constants.ts
+++
@@ -1,4 +0,0 @@
-export type GameState = "" | "X Wins!" | "O Wins!" | "Draw";
-export type CellValue = "" | "X" | "O";
-export const playerCell: CellValue = "X";
-export const aiCell: CellValue = "O";
