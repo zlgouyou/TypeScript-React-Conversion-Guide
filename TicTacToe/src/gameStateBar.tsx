3ce95e36e8ddbfbdd481baa524676f09893e26d6
Ludovic GOUYOU <ludovic.gouyou@zenika.com>
2018-09-09 22:26:22 +0200

    gameStateBar

---
+++ TicTacToe/src/gameStateBar.tsx
@@ -0,0 +1,40 @@
+import * as React from "react";
+
+const enum GameState {
+    IN_PROCESS = ""
+}
+class GameStateBarState {
+    constructor(public gameState: GameState){};
+}
+
+export class GameStateBar extends React.Component<{}, GameStateBarState> {
+    
+    constructor(props:{}) {
+        super(props);
+        this.state = new GameStateBarState(GameState.IN_PROCESS);
+    }
+    
+    private handleGameStateChange(e:CustomEvent<GameState>) {
+        this.setState({gameState: e.detail});
+    }
+  
+    private handleRestart() {
+        this.setState(new GameStateBarState(GameState.IN_PROCESS));
+    }
+
+    componentDidMount() {
+        window.addEventListener("gameStateChange", (e:CustomEvent<GameState>) => this.handleGameStateChange(e));
+        window.addEventListener("restart", () => this.handleRestart());
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener("gameStateChange", (e:CustomEvent<GameState>) => this.handleGameStateChange(e));
+        window.removeEventListener("restart", () => this.handleRestart());
+    }
+    
+    render() {
+        return (
+            <div className="gameStateBar"> {this.state.gameState} </div> 
+        )
+    }
+}   
